c8d39c2fc6d6bacbd8f447ad4c9ea08178f29167
TylerCorkill <TylerCorkill@gmail.com>
2017-10-06 15:22:41 -0700

    (feat) add route for species name

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,19 @@ Route::get('/', function () {
     return File::get(public_path() . '/index.html');
 });
 
+// get points for map
 Route::get('/api/migration/{year}/{aou}', function($year, $aou) {
     $routes = DB::select('select routes.longitude, routes.latitude, fifty.AOU from fifty, routes where fifty.year = '.$year.' AND fifty.AOU = '.$aou.' AND fifty.Route = routes.Route');
     return response($routes, 200);
 });
 
-Route::get('/api/route', function() {
-    $routes = DB::select('select * from routes');
+// get name of species with AOU
+Route::get('/api/aou/{num}', function($num) {
+    $routes = DB::select('select en_name from species where AOU = '.$num);
     return response($routes, 200);
 });
+
+
 
 Auth::routes();
 
